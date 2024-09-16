48030bdf5234c9a8302d77a9ee66ec6cfdc9074a
Liwei Ji <jiliwei.phys@gmail.com>
2024-09-16 14:45:21 -0400

    cGHG: finish calc derivatives

--- cGHG/src/rhs.cxx
+++ cGHG/src/rhs.cxx
@@ -145,53 +145,54 @@ extern "C" void cGHG_RHS(CCTK_ARGUMENTS) {
   const auto make_vec_vec_vec_gf = [&]() { return make_vec(make_vec_vec_gf); };
 
   const GF3D5<CCTK_REAL> tl_W(make_gf());
-  const vec<GF3D5<CCTK_REAL>, 3> tl_dW(make_vec_gf());
-  calcderivs(tl_W, tl_dW, gf_W);
   const GF3D5<CCTK_REAL> tl_WPi(make_gf());
-  const vec<GF3D5<CCTK_REAL>, 3> tl_dWPi(make_vec_gf());
-  calcderivs(tl_WPi, tl_dWPi, gf_WPi);
   const vec<GF3D5<CCTK_REAL>, 3> tl_WPhi(make_vec_gf());
+
+  const vec<GF3D5<CCTK_REAL>, 3> tl_dW(make_vec_gf());
+  const vec<GF3D5<CCTK_REAL>, 3> tl_dWPi(make_vec_gf());
   const vec<vec<GF3D5<CCTK_REAL>, 3>, 3> tl_dWPhi(make_vec_vec_gf());
+
+  calcderivs(tl_W, tl_dW, gf_W);
+  calcderivs(tl_WPi, tl_dWPi, gf_WPi);
   calcderivs(tl_WPhi, tl_dWPhi, gf_WPhi);
 
   const smat<GF3D5<CCTK_REAL>, 3> tl_hg(make_mat_gf());
-  const smat<vec<GF3D5<CCTK_REAL>, 3>, 3> tl_dhg(make_mat_vec_gf());
-  calcderivs(tl_hg, tl_dhg, gf_hg);
   const smat<GF3D5<CCTK_REAL>, 3> tl_hPi(make_mat_gf());
-  const smat<vec<GF3D5<CCTK_REAL>, 3>, 3> tl_dhPi(make_mat_vec_gf());
-  calcderivs(tl_hPi, tl_dhPi, gf_hPi);
   const vec<smat<GF3D5<CCTK_REAL>, 3>, 3> tl_hPhi(make_vec_mat_gf());
+
+  const smat<vec<GF3D5<CCTK_REAL>, 3>, 3> tl_dhg(make_mat_vec_gf());
+  const smat<vec<GF3D5<CCTK_REAL>, 3>, 3> tl_dhPi(make_mat_vec_gf());
   const vec<smat<vec<GF3D5<CCTK_REAL>, 3>, 3>, 3> tl_dhPhi(
       make_vec_mat_vec_gf());
+
+  calcderivs(tl_hg, tl_dhg, gf_hg);
+  calcderivs(tl_hPi, tl_dhPi, gf_hPi);
   calcderivs(tl_hPhi, tl_dhPhi, gf_hPhi);
 
   const vec<GF3D5<CCTK_REAL>, 3> tl_hgn(make_vec_gf());
-  const vec<vec<GF3D5<CCTK_REAL>, 3>, 3> tl_dhgn(make_vec_vec_gf());
-  calcderivs(tl_hgn, tl_dhgn, gf_hgn);
   const vec<GF3D5<CCTK_REAL>, 3> tl_hPin(make_vec_gf());
-  const vec<vec<GF3D5<CCTK_REAL>, 3>, 3> tl_dhPin(make_vec_vec_gf());
-  calcderivs(tl_hPin, tl_dhPin, gf_hPin);
   const vec<vec<GF3D5<CCTK_REAL>, 3>, 3> tl_hPhin(make_vec_vec_gf());
+
+  const vec<vec<GF3D5<CCTK_REAL>, 3>, 3> tl_dhgn(make_vec_vec_gf());
+  const vec<vec<GF3D5<CCTK_REAL>, 3>, 3> tl_dhPin(make_vec_vec_gf());
   const vec<vec<vec<GF3D5<CCTK_REAL>, 3>, 3>, 3> tl_dhPhin(
       make_vec_vec_vec_gf());
+
+  calcderivs(tl_hgn, tl_dhgn, gf_hgn);
+  calcderivs(tl_hPin, tl_dhPin, gf_hPin);
   calcderivs(tl_hPhin, tl_dhPhin, gf_hPhin);
 
+  const GF3D5<CCTK_REAL> tl_hgnn(make_gf());
+  const GF3D5<CCTK_REAL> tl_hPinn(make_gf());
+  const vec<GF3D5<CCTK_REAL>, 3> tl_hPhinn(make_vec_gf());
 
-  const GF3D5<CCTK_REAL> tl_exKh(make_gf());
-  const vec<GF3D5<CCTK_REAL>, 3> tl_dexKh(make_vec_gf());
-  calcderivs(tl_exKh, tl_dexKh, gf_exKh);
+  const vec<GF3D5<CCTK_REAL>, 3> tl_dhgnn(make_vec_gf());
+  const vec<GF3D5<CCTK_REAL>, 3> tl_dhPinn(make_vec_gf());
+  const vec<vec<GF3D5<CCTK_REAL>, 3>, 3> tl_dhPhinn(make_vec_vec_gf());
 
-  const smat<GF3D5<CCTK_REAL>, 3> tl_exAt(make_mat_gf());
-  const smat<vec<GF3D5<CCTK_REAL>, 3>, 3> tl_dexAt(make_mat_vec_gf());
-  calcderivs(tl_exAt, tl_dexAt, gf_exAt);
-
-  const vec<GF3D5<CCTK_REAL>, 3> tl_trGt(make_vec_gf());
-  const vec<vec<GF3D5<CCTK_REAL>, 3>, 3> tl_dtrGt(make_vec_vec_gf());
-  calcderivs(tl_trGt, tl_dtrGt, gf_trGt);
-
-  const GF3D5<CCTK_REAL> tl_Theta(make_gf());
-  const vec<GF3D5<CCTK_REAL>, 3> tl_dTheta(make_vec_gf());
-  calcderivs(tl_Theta, tl_dTheta, gf_Theta);
+  calcderivs(tl_hgnn, tl_dhgnn, gf_hgnn);
+  calcderivs(tl_hPinn, tl_dhPinn, gf_hPinn);
+  calcderivs(tl_hPhinn, tl_dhPhinn, gf_hPhinn);
 
   if (itmp != ntmps)
     CCTK_VERROR("Wrong number of temporary variables: ntmps=%d itmp=%d", ntmps,
