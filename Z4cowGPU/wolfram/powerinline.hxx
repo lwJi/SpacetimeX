d1028b4033786c21cd5b31e671312926a23bbe8f
Liwei Ji <jiliwei.phys@gmail.com>
2025-02-12 10:47:37 -0500

    Z4cowGPU: update powerinline.hxx

--- Z4cowGPU/wolfram/powerinline.hxx
+++ Z4cowGPU/wolfram/powerinline.hxx
@@ -9,27 +9,24 @@
 
 namespace Z4cowGPU {
 
-template <typename T> constexpr T Power(T base, int exp) {
-  static_assert(std::is_arithmetic<T>::value,
-                "Power function only supports arithmetic types.");
-
-  if constexpr (std::is_floating_point<T>::value) {
-    return std::pow(base, exp); // Use std::pow for floating-point types
-  } else {
-    if (exp < 0) {
-      return static_cast<T>(1) /
-             Power(
-                 base,
-                 -exp); // Handle negative exponent for non-floating point types
+template <typename T, typename U> inline T Power(T x, U y) {
+  // If y is an integer, use exponentiation by squaring
+  if constexpr (std::is_integral<U>::value) {
+    if (y < 0) {
+      return static_cast<T>(1) / Power(x, -y); // Handle negative exponents
     }
     T result = 1;
-    while (exp) {
-      if (exp % 2)
-        result *= base;
-      base *= base;
-      exp /= 2;
+    while (y) {
+      if (y % 2) {
+        result *= x;
+      }
+      x *= x;
+      y /= 2;
     }
     return result;
+  } else {
+    // Use std::pow for non-integer exponents
+    return std::pow(x, y);
   }
 }
 
