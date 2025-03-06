8ff0a65ae520c440da6e45f733a53cca07bb4f09
Liwei Ji <jiliwei.phys@gmail.com>
2025-03-05 20:43:57 -0500

    TestSTXUtils: update test.cxx

--- TestSTXUtils/src/test.cxx
+++ TestSTXUtils/src/test.cxx
@@ -6,19 +6,24 @@
 #include <cctk_Arguments.h>
 #include <cctk_Parameters.h>
 
+#include <stx_derivsGF3D5.hxx>
+#include <stx_dissinline.hxx>
+#include <stx_powerinline.hxx>
+
 namespace TestSTXUtils {
 using namespace Loop;
+using namespace STXUtils;
 
-template <typename T, typename VT>
-constexpr VT poly(const T kxx, const T kxy, const T kyz, const VT x, const T y,
-                  const T z) {
+template <typename T>
+constexpr T poly(const T kxx, const T kxy, const T kyz, const T x, const T y,
+                 const T z) {
   return kxx * x * x + kxy * x * y + kyz * y * z;
 }
 
 template <typename T>
 constexpr void poly_derivs(const T kxx, const T kxy, const T kyz, const T x,
-                           const T y, const T z, Arith::vec<T, dim> &du,
-                           Arith::smat<T, dim> &ddu) {
+                           const T y, const T z, std::array<T, 3> &du,
+                           std::array<T, 6> &ddu) {
   using std::sin, std::cos, std::sqrt;
   const T sinx = sin(x);
   const T siny = sin(y);
@@ -26,25 +31,15 @@ constexpr void poly_derivs(const T kxx, const T kxy, const T kyz, const T x,
   const T cosx = cos(x);
   const T cosy = cos(y);
   const T cosz = cos(z);
-  du(0) = -2 * kxx * cosx * sinx - kxy * sinx * siny;
-  du(1) = kxy * cosx * cosy + kyz * cosy * sinz;
-  du(2) = kyz * cosz * siny;
-  ddu(0, 0) =
-      -2 * kxx * cosx * cosx + 2 * kxx * sinx * sinx - kxy * cosx * siny;
-  ddu(0, 1) = -kxy * cosy * sinx;
-  ddu(0, 2) = 0.0;
-  ddu(1, 1) = -kxy * cosx * siny - kyz * siny * sinz;
-  ddu(1, 2) = kyz * cosy * cosz;
-  ddu(2, 2) = -kyz * siny * sinz;
-}
-
-template <typename T>
-constexpr Arith::vec<T, dim> beta_profile(const T x, const T y, const T z) {
-  const T vx = 1 * sin(x);
-  const T vy = 2 * sin(y);
-  const T vz = 3 * sin(z);
-  const T norm = sqrt(vx * vx + vy * vy + vz * vz);
-  return Arith::vec<T, dim>({vx / norm, vy / norm, vy / norm});
+  du[0] = -2 * kxx * cosx * sinx - kxy * sinx * siny;
+  du[1] = kxy * cosx * cosy + kyz * cosy * sinz;
+  du[2] = kyz * cosz * siny;
+  ddu[0] = -2 * kxx * cosx * cosx + 2 * kxx * sinx * sinx - kxy * cosx * siny;
+  ddu[1] = -kxy * cosy * sinx;
+  ddu[2] = 0.0;
+  ddu[3] = -kxy * cosx * siny - kyz * siny * sinz;
+  ddu[4] = kyz * cosy * cosz;
+  ddu[5] = -kyz * siny * sinz;
 }
 
 template <typename T>
@@ -102,28 +97,11 @@ extern "C" void TestSTXUtils_Set(CCTK_ARGUMENTS) {
   DECLARE_CCTK_ARGUMENTSX_TestSTXUtils_Set;
   DECLARE_CCTK_PARAMETERS;
 
-  using std::sin;
-  using vreal = Arith::simd<CCTK_REAL>;
-  using vbool = Arith::simdl<CCTK_REAL>;
-  constexpr std::size_t vsize = std::tuple_size_v<vreal>;
-
-  grid.loop_int_device<0, 0, 0, vsize>(
-      grid.nghostzones,
-      [=] CCTK_DEVICE(const PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
-        const vbool mask = Arith::mask_for_loop_tail<vbool>(p.i, p.imax);
-        const vreal x0 = p.x + Arith::iota<vreal>() * p.dx;
-        const CCTK_REAL y0 = p.y;
-        const CCTK_REAL z0 = p.z;
-        chi.store(mask, p.I, poly(kxx, kxy, kyz, cos(x0), sin(y0), sin(z0)));
-      });
-
+  using std::sin, std::cos;
   grid.loop_int_device<0, 0, 0>(
       grid.nghostzones,
       [=] CCTK_DEVICE(const PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
-        const Arith::vec<CCTK_REAL, dim> betas = beta_profile(p.x, p.y, p.z);
-        betax(p.I) = betas(0);
-        betay(p.I) = betas(1);
-        betaz(p.I) = betas(2);
+        chi(p.I) = poly(kxx, kxy, kyz, cos(p.x), sin(p.y), sin(p.z));
       });
 }
 
@@ -132,176 +110,123 @@ extern "C" void TestSTXUtils_Sync(CCTK_ARGUMENTS) {
   // do nothing
 }
 
-extern "C" void TestSTXUtils_CalcSTXUtils(CCTK_ARGUMENTS) {
-  DECLARE_CCTK_ARGUMENTS_TestSTXUtils_CalcSTXUtils;
+extern "C" void TestSTXUtils_CalcDerivs(CCTK_ARGUMENTS) {
+  DECLARE_CCTK_ARGUMENTS_TestSTXUtils_CalcDerivs;
   DECLARE_CCTK_PARAMETERS;
 
-  const std::array<int, dim> indextype = {0, 0, 0};
-  const std::array<int, dim> nghostzones = {
-      cctk_nghostzones[0], cctk_nghostzones[1], cctk_nghostzones[2]};
-  Arith::vect<int, dim> imin, imax;
-  GridDescBase(cctkGH).box_int<0, 0, 0>(nghostzones, imin, imax);
-  // Suffix 2: with ghost zones, suffix 5: without ghost zones
-  const GF3D2layout layout2(cctkGH, indextype);
-  const GF3D5layout layout5(imin, imax);
-  const GF3D2<const CCTK_REAL> gf2_chi(layout2, chi);
+  for (int d = 0; d < 3; ++d)
+    if (cctk_nghostzones[d] < deriv_order / 2)
+      CCTK_VERROR("Need at least %d ghost zones", deriv_order / 2);
+
+  const array<CCTK_REAL, 3> invDxyz{1. / CCTK_DELTA_SPACE(0),
+                                    1. / CCTK_DELTA_SPACE(1),
+                                    1. / CCTK_DELTA_SPACE(2)};
+
+  const GF3D2layout layout2(cctkGH, {0, 0, 0});
+
+  const CCTK_REAL *gf_chi = chi;
 
   /* Define temp tail vars */
-  constexpr int nvars = 10;
-  GF3D5vector<CCTK_REAL> vars(layout5, nvars);
-
+  constexpr int nvars = 9;
   int ivar = 0;
-  const auto make_gf = [&]() { return GF3D5<CCTK_REAL>(vars(ivar++)); };
-  const auto make_vec = [&](const auto &f) {
-    return Arith::vec<std::result_of_t<decltype(f)()>, dim>(
-        [&](int) { return f(); });
-  };
-  const auto make_mat = [&](const auto &f) {
-    return Arith::smat<std::result_of_t<decltype(f)()>, dim>(
-        [&](int, int) { return f(); });
-  };
-  const auto make_vec_gf = [&]() { return make_vec(make_gf); };
-  const auto make_mat_gf = [&]() { return make_mat(make_gf); };
-
-  const GF3D5<CCTK_REAL> t5_chi(make_gf());
-  const Arith::vec<GF3D5<CCTK_REAL>, dim> t5_dchi(make_vec_gf());
-  const Arith::smat<GF3D5<CCTK_REAL>, dim> t5_ddchi(make_mat_gf());
-
-  const GridDescBaseDevice grid(cctkGH);
-  const Arith::vect<CCTK_REAL, dim> dx(std::array<CCTK_REAL, dim>{
-      CCTK_DELTA_SPACE(0),
-      CCTK_DELTA_SPACE(1),
-      CCTK_DELTA_SPACE(2),
-  });
-
-  STXUtils::calc_derivs2<0, 0, 0>(t5_chi, t5_dchi, t5_ddchi, layout5, grid,
-                                gf2_chi, dx, deriv_order);
+  const GF3D5layout layout5 = get_GF3D5layout<0, 0, 0>(cctkGH);
+  GF3D5Factory<CCTK_REAL> fct(layout5, nvars, ivar);
+  const auto t5_dchi = fct.make_vec_gf();
+  const auto t5_ddchi = fct.make_smat_gf();
+  if (ivar != nvars)
+    CCTK_VERROR("Wrong number of temporary variables: nvars=%d ivar=%d", nvars,
+                ivar);
   ivar = -1;
 
-  const Arith::vec<GF3D2<CCTK_REAL>, dim> gf_dchi{
-      GF3D2<CCTK_REAL>(layout2, dxchi), GF3D2<CCTK_REAL>(layout2, dychi),
-      GF3D2<CCTK_REAL>(layout2, dzchi)};
-  const Arith::smat<GF3D2<CCTK_REAL>, dim> gf_ddchi{
-      GF3D2<CCTK_REAL>(layout2, dxxchi), GF3D2<CCTK_REAL>(layout2, dxychi),
-      GF3D2<CCTK_REAL>(layout2, dxzchi), GF3D2<CCTK_REAL>(layout2, dyychi),
-      GF3D2<CCTK_REAL>(layout2, dyzchi), GF3D2<CCTK_REAL>(layout2, dzzchi)};
-  const GF3D2<CCTK_REAL> gf_chi_diss(layout2, chi_diss);
+  // Define derivs lambdas
+  const auto calcderivs2nd = [&](const auto &dgf, const auto &ddgf,
+                                 const auto &gf_) {
+    calc_derivs2nd<0, 0, 0>(grid, layout5, dgf, ddgf, layout2, gf_, invDxyz,
+                            deriv_order);
+  };
 
-  const Arith::vec<GF3D2<const CCTK_REAL>, 3> gf_beta{
-      GF3D2<const CCTK_REAL>(layout2, betax),
-      GF3D2<const CCTK_REAL>(layout2, betay),
-      GF3D2<const CCTK_REAL>(layout2, betaz)};
-  const GF3D2<CCTK_REAL> gf_chi_upwind(layout2, chi_upwind);
+  calcderivs2nd(tl_dchi, tl_ddchi, gf_chi);
 
-  typedef Arith::simd<CCTK_REAL> vreal;
-  typedef Arith::simdl<CCTK_REAL> vbool;
-  constexpr size_t vsize = std::tuple_size_v<vreal>;
+  const GridDescBaseDevice grid(cctkGH);
 
-  switch (deriv_order) {
-  case 2: {
-    grid.loop_int_device<0, 0, 0, vsize>(
-        grid.nghostzones, [=] ARITH_DEVICE(const PointDesc &p) ARITH_INLINE {
-          const vbool mask = Arith::mask_for_loop_tail<vbool>(p.i, p.imax);
-          const GF3D2index index2(layout2, p.I);
-          const GF3D5index index5(layout5, p.I);
-          gf_dchi.store(mask, index2, t5_dchi(mask, index5));
-          gf_ddchi.store(mask, index2, t5_ddchi(mask, index5));
-          gf_chi_diss.store(mask, index2,
-                            STXUtils::calc_diss<2>(gf2_chi, mask, p.I, dx));
-          gf_chi_upwind.store(
-              mask, index2,
-              STXUtils::calc_deriv_upwind<2>(gf2_chi, mask, p.I, dx,
-                                           gf_beta(mask, index2)));
-        });
-    break;
-  }
-  case 4: {
-    grid.loop_int_device<0, 0, 0, vsize>(
-        grid.nghostzones, [=] ARITH_DEVICE(const PointDesc &p) ARITH_INLINE {
-          const vbool mask = Arith::mask_for_loop_tail<vbool>(p.i, p.imax);
-          const GF3D2index index2(layout2, p.I);
-          const GF3D5index index5(layout5, p.I);
-          gf_dchi.store(mask, index2, t5_dchi(mask, index5));
-          gf_ddchi.store(mask, index2, t5_ddchi(mask, index5));
-          gf_chi_diss.store(mask, index2,
-                            STXUtils::calc_diss<4>(gf2_chi, mask, p.I, dx));
-          gf_chi_upwind.store(
-              mask, index2,
-              STXUtils::calc_deriv_upwind<4>(gf2_chi, mask, p.I, dx,
-                                           gf_beta(mask, index2)));
-        });
-    break;
-  }
-  case 6: {
-    grid.loop_int_device<0, 0, 0, vsize>(
-        grid.nghostzones, [=] ARITH_DEVICE(const PointDesc &p) ARITH_INLINE {
-          const vbool mask = Arith::mask_for_loop_tail<vbool>(p.i, p.imax);
-          const GF3D2index index2(layout2, p.I);
-          const GF3D5index index5(layout5, p.I);
-          gf_dchi.store(mask, index2, t5_dchi(mask, index5));
-          gf_ddchi.store(mask, index2, t5_ddchi(mask, index5));
-          gf_chi_diss.store(mask, index2,
-                            STXUtils::calc_diss<6>(gf2_chi, mask, p.I, dx));
-          gf_chi_upwind.store(
-              mask, index2,
-              STXUtils::calc_deriv_upwind<6>(gf2_chi, mask, p.I, dx,
-                                           gf_beta(mask, index2)));
-        });
-    break;
-  }
-  case 8: {
-    grid.loop_int_device<0, 0, 0, vsize>(
-        grid.nghostzones, [=] ARITH_DEVICE(const PointDesc &p) ARITH_INLINE {
-          const vbool mask = Arith::mask_for_loop_tail<vbool>(p.i, p.imax);
-          const GF3D2index index2(layout2, p.I);
-          const GF3D5index index5(layout5, p.I);
-          gf_dchi.store(mask, index2, t5_dchi(mask, index5));
-          gf_ddchi.store(mask, index2, t5_ddchi(mask, index5));
-          gf_chi_diss.store(mask, index2,
-                            STXUtils::calc_diss<8>(gf2_chi, mask, p.I, dx));
-          gf_chi_upwind.store(
-              mask, index2,
-              STXUtils::calc_deriv_upwind<8>(gf2_chi, mask, p.I, dx,
-                                           gf_beta(mask, index2)));
-        });
-    break;
-  }
-  default:
-    assert(0);
-  }
-
-#if CCTK_DEBUG
   grid.loop_int_device<0, 0, 0>(
       grid.nghostzones, [=] ARITH_DEVICE(const PointDesc &p) ARITH_INLINE {
-        Arith::vec<CCTK_REAL, dim> dchi;
-        Arith::smat<CCTK_REAL, dim> ddchi;
-        poly_derivs(kxx, kxy, kyz, p.x, p.y, p.z, dchi, ddchi);
-
-        const auto tiny = 5e-3;
-        if (dchi(0) - gf_dchi(0)(p.I) > tiny ||
-            dchi(1) - gf_dchi(1)(p.I) > tiny ||
-            dchi(2) - gf_dchi(2)(p.I) > tiny ||
-            ddchi(0, 0) - gf_ddchi(0, 0)(p.I) > tiny ||
-            ddchi(0, 1) - gf_ddchi(0, 1)(p.I) > tiny ||
-            ddchi(0, 2) - gf_ddchi(0, 2)(p.I) > tiny ||
-            ddchi(1, 1) - gf_ddchi(1, 1)(p.I) > tiny ||
-            ddchi(1, 2) - gf_ddchi(1, 2)(p.I) > tiny ||
-            ddchi(2, 2) - gf_ddchi(2, 2)(p.I) > tiny) {
-#ifndef SYCL_LANGUAGE_VERSION
-          printf("dx = %f\n", dchi(0) - gf_dchi(0)(p.I));
-          printf("dy = %f\n", dchi(1) - gf_dchi(1)(p.I));
-          printf("dz = %f\n", dchi(2) - gf_dchi(2)(p.I));
-          printf("ddxx = %f\n", ddchi(0, 0) - gf_ddchi(0, 0)(p.I));
-          printf("ddxy = %f\n", ddchi(0, 1) - gf_ddchi(0, 1)(p.I));
-          printf("ddxz = %f\n", ddchi(0, 2) - gf_ddchi(0, 2)(p.I));
-          printf("ddyy = %f\n", ddchi(1, 1) - gf_ddchi(1, 1)(p.I));
-          printf("ddyz = %f\n", ddchi(1, 2) - gf_ddchi(1, 2)(p.I));
-          printf("ddzz = %f\n", ddchi(2, 2) - gf_ddchi(2, 2)(p.I));
-#endif
-          assert(0);
-        }
+        const int ijk = layout2.linear(p.i, p.j, p.k);
+        const int ijk5 = layout5.linear(p.i, p.j, p.k);
+        dxchi[ijk] = tl_dchi[0].ptr[ijk5];
+        dychi[ijk] = tl_dchi[1].ptr[ijk5];
+        dzchi[ijk] = tl_dchi[2].ptr[ijk5];
+        dxxchi[ijk] = tl_ddchi[0].ptr[ijk5];
+        dxychi[ijk] = tl_ddchi[1].ptr[ijk5];
+        dxzchi[ijk] = tl_ddchi[2].ptr[ijk5];
+        dyychi[ijk] = tl_ddchi[3].ptr[ijk5];
+        dyzchi[ijk] = tl_ddchi[4].ptr[ijk5];
+        dzzchi[ijk] = tl_ddchi[5].ptr[ijk5];
       });
-#endif
+}
+
+extern "C" void TestSTXUtils_CalcDiss(CCTK_ARGUMENTS) {
+  DECLARE_CCTK_ARGUMENTS_TestSTXUtils_CalcDiss;
+  DECLARE_CCTK_PARAMETERS;
+
+  for (int d = 0; d < 3; ++d)
+    if (cctk_nghostzones[d] < deriv_order / 2)
+      CCTK_VERROR("Need at least %d ghost zones", deriv_order / 2);
+
+  const array<CCTK_REAL, 3> invDxyz{1. / CCTK_DELTA_SPACE(0),
+                                    1. / CCTK_DELTA_SPACE(1),
+                                    1. / CCTK_DELTA_SPACE(2)};
+
+  const GF3D2layout layout2(cctkGH, {0, 0, 0});
+
+  const CCTK_REAL *gf_chi = chi;
+
+  const int diss_order = deriv_order + 2;
+
+  const auto calcdiss = [&](const CCTK_REAL *gf_, CCTK_REAL *gf_rhs_) {
+    switch (diss_order) {
+    case 4: {
+      grid.loop_int_device<0, 0, 0>(
+          grid.nghostzones,
+          [=] CCTK_DEVICE(const PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
+            const int ijk = layout2.linear(p.i, p.j, p.k);
+            gf_rhs_[ijk] = calc_diss<2>(layout2, gf_, p.i, p.j, p.k, invDxyz);
+          });
+      break;
+    }
+    case 6: {
+      grid.loop_int_device<0, 0, 0>(
+          grid.nghostzones,
+          [=] CCTK_DEVICE(const PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
+            const int ijk = layout2.linear(p.i, p.j, p.k);
+            gf_rhs_[ijk] = calc_diss<4>(layout2, gf_, p.i, p.j, p.k, invDxyz);
+          });
+      break;
+    }
+    case 8: {
+      grid.loop_int_device<0, 0, 0>(
+          grid.nghostzones,
+          [=] CCTK_DEVICE(const PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
+            const int ijk = layout2.linear(p.i, p.j, p.k);
+            gf_rhs_[ijk] = calc_diss<6>(layout2, gf_, p.i, p.j, p.k, invDxyz);
+          });
+      break;
+    }
+    case 10: {
+      grid.loop_int_device<0, 0, 0>(
+          grid.nghostzones,
+          [=] CCTK_DEVICE(const PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
+            const int ijk = layout2.linear(p.i, p.j, p.k);
+            gf_rhs_[ijk] = calc_diss<8>(layout2, gf_, p.i, p.j, p.k, invDxyz);
+          });
+      break;
+    }
+    default:
+      assert(0);
+    }
+  };
+
+  calcdiss(chi, chi_diss);
 }
 
 extern "C" void TestSTXUtils_CalcError(CCTK_ARGUMENTS) {
@@ -316,30 +241,28 @@ extern "C" void TestSTXUtils_CalcError(CCTK_ARGUMENTS) {
 
   grid.loop_int_device<0, 0, 0>(
       grid.nghostzones, [=] ARITH_DEVICE(const PointDesc &p) ARITH_INLINE {
-        Arith::vec<CCTK_REAL, dim> dchi;
-        Arith::smat<CCTK_REAL, dim> ddchi;
+        // errors in derivs
+        std::array<CCTK_REAL, 3> dchi;
+        std::array<CCTK_REAL, 6> ddchi;
         poly_derivs(kxx, kxy, kyz, p.x, p.y, p.z, dchi, ddchi);
 
-        dxchi_error(p.I) = dxchi(p.I) - dchi(0);
-        dychi_error(p.I) = dychi(p.I) - dchi(1);
-        dzchi_error(p.I) = dzchi(p.I) - dchi(2);
-        dxxchi_error(p.I) = dxxchi(p.I) - ddchi(0, 0);
-        dxychi_error(p.I) = dxychi(p.I) - ddchi(0, 1);
-        dxzchi_error(p.I) = dxzchi(p.I) - ddchi(0, 2);
-        dyychi_error(p.I) = dyychi(p.I) - ddchi(1, 1);
-        dyzchi_error(p.I) = dyzchi(p.I) - ddchi(1, 2);
-        dzzchi_error(p.I) = dzzchi(p.I) - ddchi(2, 2);
+        dxchi_error(p.I) = dxchi(p.I) - dchi[0];
+        dychi_error(p.I) = dychi(p.I) - dchi[1];
+        dzchi_error(p.I) = dzchi(p.I) - dchi[2];
+        dxxchi_error(p.I) = dxxchi(p.I) - ddchi[0];
+        dxychi_error(p.I) = dxychi(p.I) - ddchi[1];
+        dxzchi_error(p.I) = dxzchi(p.I) - ddchi[2];
+        dyychi_error(p.I) = dyychi(p.I) - ddchi[3];
+        dyzchi_error(p.I) = dyzchi(p.I) - ddchi[4];
+        dzzchi_error(p.I) = dzzchi(p.I) - ddchi[5];
 
+        // errors in diss
         const int diss_order = deriv_order + 2;
+
         const CCTK_REAL diss =
-            Arith::pown(-1, diss_order / 2 - 1) / Arith::pown(2, diss_order) *
+            Power(-1, diss_order / 2 - 1) / Power(2, diss_order) *
             poly_diss(kxx, kxy, kyz, p.x, p.y, p.z, diss_order, dx);
         chi_diss_error(p.I) = diss - chi_diss(p.I);
-
-        const Arith::vec<CCTK_REAL, dim> betas = beta_profile(p.x, p.y, p.z);
-        const CCTK_REAL upwind = Arith::sum<dim>(
-            [&](int i) ARITH_INLINE { return betas(i) * dchi(i); });
-        chi_upwind_error(p.I) = upwind - chi_upwind(p.I);
       });
 }
 
