96692892fb4f50d52f76af933466d7fd3a47ae4d
Liwei Ji <jiliwei.phys@gmail.com>
2025-05-30 13:55:04 -0400

    WeylScalo: add weylscalo.cxx

---
+++ WeylScalo/src/weylscalo.cxx
@@ -0,0 +1,60 @@
+#include <cctk.h>
+#include <cctk_Arguments.h>
+#include <cctk_Parameters.h>
+#include <loop_device.hxx>
+#include <stx_utils.hxx>
+
+#include <cmath>
+
+#include <stx_derivsGF3D5.hxx>
+#include <stx_derivsinline.hxx>
+#include <stx_dissinline.hxx>
+#include <stx_powerinline.hxx>
+
+namespace WeylScalo {
+using namespace Arith;
+using namespace Loop;
+using namespace STXUtils;
+
+extern "C" void WeylScalo_calc_psi4(CCTK_ARGUMENTS) {
+  DECLARE_CCTK_ARGUMENTS_WeylScalo_calc_psi4;
+  DECLARE_CCTK_PARAMETERS;
+
+  for (int d = 0; d < 3; ++d)
+    if (cctk_nghostzones[d] < deriv_order / 2)
+      CCTK_VERROR("Need at least %d ghost zones", deriv_order / 2);
+
+  const array<CCTK_REAL, 3> invDxyz{1. / CCTK_DELTA_SPACE(0),
+                                    1. / CCTK_DELTA_SPACE(1),
+                                    1. / CCTK_DELTA_SPACE(2)};
+
+  const GF3D2layout layout2(cctkGH, {0, 0, 0});
+
+  // Input grid functions
+  const array<const CCTK_REAL *, 6> gf_gam{gxx, gxy, gxz, gyy, gyz, gzz};
+  const array<const CCTK_REAL *, 6> gf_exK{Kxx, Kxy, Kxz, Kyy, Kyz, Kzz};
+  const CCTK_REAL *gf_alpha = alp;
+  const array<const CCTK_REAL *, 3> gf_beta{betaGx, betaGy, betaGz};
+
+  // Output grid functions
+  const CCTK_REAL *gf_Psi4real = Psi4r;
+  const CCTK_REAL *gf_Psi4imag = Psi4i;
+
+  // Parameters
+  const CCTK_REAL cpi = M_PI;
+
+  // Loop
+  const Loop::GridDescBaseDevice grid(cctkGH);
+
+  // Derivs Lambdas
+#include "../wolfram/WeylScalo_derivs1st.hxx"
+#include "../wolfram/WeylScalo_derivs2nd.hxx"
+
+#include "../wolfram/WeylScalo_set_rhs.hxx"
+}
+
+extern "C" void WeylScalo_Sync(CCTK_ARGUMENTS) {
+  // do nothing
+}
+
+} // namespace WeylScalo
